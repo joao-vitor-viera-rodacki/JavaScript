554edee0b1c26c41eb07b53e2129e2b963bacd98
joao-vitor-viera-rodacki <joao.vie.rodacki@gmail.com>
2022-07-15 19:17:21 -0300

    Created

---
+++ Modulo B/ex003/ex001.js
@@ -0,0 +1,12 @@
+var n = 1
+n = n.toString()
+alert(n)
+var s  = 'javascript'
+s.length
+s.toUpperCase()
+s.toLowerCase()
+//numeros
+var n1 =1545
+n1.toFixed(2)
+n1.toFixed(2).replace('.',',')
+n1.toLocaleString('pr-br',{style: 'currency',currency : 'BRL'})
